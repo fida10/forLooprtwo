dd680ea8c41681653203afc98cf0942dd23a9f0d
Eshad <fidaeshad10@gmail.com>
2018-12-16 15:54:39 -0500

    the first committment woo woo woo

---
+++ src/firstpack/Main.java
@@ -0,0 +1,20 @@
+package firstpack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("For loop test");
+
+        for(int j = 25; j >= -3; j--) {
+            System.out.println("the value of J is currently: " + j);
+            System.out.println("Going down one level");
+        }
+
+        System.out.println("Looks like we can go no lower!");
+
+
+    }
+
+}
